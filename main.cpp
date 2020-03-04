602a241d49da651ae3b3bf56bdf35eb94d6c8b7a
Nick Seroni <seroninick108@gmail.com>
2020-03-04 12:53:30 -0600

    Created function protoypes in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+// Conversion functions
+bool characteristic(char numString[], int& c);
+bool mantissa(char numString[], int& numerator, int& denominator);
+
+// Math functions
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+
 int main()
 {
     cout<<"In-class Code Review"<<endl;
